c9d16d8876a9697ce7b14e74dc50610e91035a25
Utkarsh123 <usjadhav8495@gmail.com>
2022-09-14 19:42:08 +0530

    This is First prog

---
+++ src/my1stprogram/Mysecond.java
@@ -0,0 +1,14 @@
+package my1stprogram;
+
+public class Mysecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		System.out.println("This second prog i added");
+	}
+
+}
